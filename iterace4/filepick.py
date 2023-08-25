c31ac637737bad9477fe53eb701c97c293004418
Ondřej Marek <xmarek9@fi.muni.cz>
2023-08-25 23:39:16 +0200

    iterace4

---
+++ iterace4/filepick.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Přidáme přepínač -r, který bude určovat, jestli má kopírování proběhnout rekurzivně (pokud bude přítomen).
+To znamená že se bude prohledávat celý adresářový strom. Jinak se bude skript chovat jako v 1. iteraci,
+tzn. že bude prohledávat soubory jen v našem adresáři.
+
+Dále předělejte skript tak, aby při procházení souborů používal knihovnu pathlib.
+
+Bude se hodit funkce adresar.iterdir() - ta vrati vsechny polozky v danem adresari
+soubor.stat().st_ctime - vraci datum vytvoreni
+"""
+
+import os
+import sys
+import shutil
+from pathlib import Path
+from datetime import datetime
+
+
+def parse_args():
+    pass
+
+
+def copy_files(source_dir: Path, dest_dir: Path, file_exts, date, recursive):
+    pass
+
+
+def filepick():
+    src, dest, file_exts, date, recursive = parse_args()
+    copy_files(Path(src), Path(dest), file_exts, date.timestamp(), recursive)
+
+
+if __name__ == '__main__':
+    filepick()
---
+++ iterace4/test_filepick.py
@@ -0,0 +1,80 @@
+from typing import Set
+
+import filepick
+import sys
+import os
+import shutil
+
+"""
+Spusť tenhle skript pro kontrolu řešení.
+"""
+
+TARGET_DIR = "target"
+
+
+def make_dirtree():
+    os.makedirs(os.path.join("test", "1", "2"))
+    os.makedirs(os.path.join("test", "1", "3"))
+    os.makedirs(os.path.join("test", "4"))
+
+    with open(os.path.join("test", "1", "a.txt"), "w"):
+        pass
+    with open(os.path.join("test", "1", "b.png"), "w"):
+        pass
+    with open(os.path.join("test", "1", "c.docx"), "w"):
+        pass
+    with open(os.path.join("test", "1", "2", "yeehaw.txt"), "w"):
+        pass
+    with open(os.path.join("test", "1", "3", "42.txt"), "w"):
+        pass
+    with open(os.path.join("test", "4", "d.txt"), "w"):
+        pass
+    with open(os.path.join("test", "4", "e.wtf"), "w"):
+        pass
+
+
+def test_filepick(source: str, correct_files: Set[str]) -> None:
+    os.makedirs(TARGET_DIR)
+    sys.argv[1] = source
+    sys.argv[2] = TARGET_DIR
+    filepick.filepick()
+
+    assert set(os.listdir(TARGET_DIR)) == correct_files, \
+        f"Neshodují se zkopírované soubory - zkopírovalo se {set(os.listdir(TARGET_DIR))}, správně " \
+        f"by se mělo zkopírovat {correct_files}"
+
+    shutil.rmtree(TARGET_DIR)
+
+
+def test():
+    sys.argv = []
+    sys.argv.append("filepick.py")
+    sys.argv.append("filepick.py")
+    sys.argv.append("filepick.py")
+    make_dirtree()
+
+    test_filepick(os.path.join("test", "1"), {'a.txt', 'c.docx'})
+
+    sys.argv.append("filepick.py")
+    sys.argv[3] = "-r"
+
+    test_filepick(os.path.join("test", "1"), {'a.txt', 'c.docx', 'yeehaw.txt', '42.txt'})
+    test_filepick(os.path.join("test", "1", "2"), {'yeehaw.txt'})
+    test_filepick(os.path.join("test", "1", "3"), {'42.txt'})
+    test_filepick(os.path.join("test", "4"), {'d.txt'})
+    test_filepick(os.path.join("test"), {'a.txt', 'c.docx', 'yeehaw.txt', '42.txt', 'd.txt'})
+
+    sys.argv.append("filepick.py")
+    sys.argv[3] = ".wtf"
+    sys.argv[4] = ".png"
+    test_filepick(os.path.join("test"), set())
+
+    sys.argv.append("filepick.py")
+    sys.argv[5] = "-r"
+    test_filepick(os.path.join("test"), {"e.wtf", "b.png"})
+
+    shutil.rmtree("test")
+
+
+if __name__ == '__main__':
+    test()
